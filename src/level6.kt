69efec7b43baa335c7fc7561ea03cfa2ab223b5c
Andrei <actsituation@gmail.com>
2020-02-27 20:21:58 +0700

    Completed sixth level=)

--- src/level6.kt
+++ src/level6.kt
@@ -1,3 +1,13 @@
-fun main(words: Array<String>) {
-    //TODO
+fun main() {
+    val scan = java.util.Scanner(System.`in`)
+    val words = scan.nextLine().split(" ")
+
+    val sorted_freq = words.groupingBy {it}
+        .eachCount()
+        .toList()
+        .sortedBy { (key) -> key }
+        .sortedByDescending { (value) -> value }
+
+    for ((k, v) in sorted_freq)
+        println("$k $v")
 }
