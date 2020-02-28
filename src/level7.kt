fc28fc6d50ec8fe39a880d4d9138ab04a0cbcd7e
Andrei <actsituation@gmail.com>
2020-02-28 20:47:21 +0700

    Add ternary to seventh level

--- src/level7.kt
+++ src/level7.kt
@@ -1,3 +1,24 @@
-fun main() {
-    //TODO
+import java.util.Scanner
+
+
+fun main(args: Array<String>) {
+
+    val words: Array<String> = (
+        if (args.isEmpty()) {
+            Scanner(System.`in`).nextLine()
+                .split(" ")
+                .toTypedArray()
+        }
+        else args
+    )
+
+
+    val sorted_freq: List<Pair<String, Int>> = words.groupingBy {it}
+            .eachCount()
+            .toList()
+            .sortedBy { (key) -> key }
+            .sortedByDescending { (value) -> value }
+
+    for ((k, v) in sorted_freq)
+        println("$k $v")
 }
